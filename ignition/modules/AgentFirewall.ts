26c5a7af97b99549142097004b14752354cd65c8
dtucker <davidt2003@msn.com>
2026-04-04 12:28:40 +0200

    chore: update Ignition module for new constructor with NameWrapper

--- ignition/modules/AgentFirewall.ts
+++ ignition/modules/AgentFirewall.ts
@@ -2,9 +2,11 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("AgentFirewall", (m) => {
   const ensResolver = m.getParameter("ensResolver");
+  const nameWrapper = m.getParameter("nameWrapper");
+  const ensParentNode = m.getParameter("ensParentNode");
   const forwarder = m.getParameter("forwarder");
 
-  const firewall = m.contract("AgentFirewall", [ensResolver, forwarder]);
+  const firewall = m.contract("AgentFirewall", [ensResolver, nameWrapper, ensParentNode, forwarder]);
 
   return { firewall };
 });
